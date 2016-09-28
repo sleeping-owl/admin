ce4ff26ac31c5817ee0f8d5bcf6c15d76b8d2d84
labapawel@gmail.com <parsuj@debian>
2016-09-28 06:30:33 +0200

    The addition of Polish language

---
+++ src/lang/pl/validation.php
@@ -0,0 +1,68 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Validation Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| Pole following language lines contain the default error messages used by
+	| the validator class. Some of these rules have multiple versions such
+	| as the size rules. Feel free to tweak each of these messages here.
+	|
+	*/
+
+	"accepted"                => "pole :attribute powinno zostać zaakceptowane.",
+	"active_url"              => "pole :attribute posiada złą wartość URL.",
+	"after"                   => "pole :attribute jest datą po :date.",
+	"alpha"                   => "pole :attribute powinno zawierać tylko litery.",
+	"alpha_dash"              => "pole :attribute powinno zawierać litery, cyfry i myślniki.",
+	"alpha_num"               => "pole :attribute powinno zawierać litery i cyfry.",
+	"array"                   => "pole :attribute powinno być tablicą.",
+	"before"                  => "Pole :attribute data powinna być przed :date.",
+	"confirmed"               => "Pola :attribute nie są takie same.",
+	"date"                    => "Pole :attribute nie jest datą.",
+	"date_format"             => "Pole :attribute powinno zawierać format :format.",
+	"different"               => "Pole :attribute i :other powinny być różne.",
+	"digits"                  => "Pole :attribute powinno mieć :digits cyfr.",
+	"digits_between"          => "Pole :attribute powinno zawierać pomiędzy :min i :max cyfr.",
+	"email"                   => "Pole :attribute powinno być adresem email.",
+	"exists"                  => "Wartość pola :attribute istnieje.",
+	"image"                   => "Pole :attribute powinno być obrazem.",
+	"in"                      => "W pole :attribute zaznacz więcej elementów.",
+	"integer"                 => "Pole :attribute powinno być liczbą całkowitą.",
+	"ip"                      => "Pole :attribute Zawiera błędny adres IP.",
+	"mimes"                   => "Plik :attribute powinien być typu: :values.",
+	"not_in"                  => "Pole :attribute nie zawiera .",
+	"not_php"                 => "Zły typ pliku.",
+	"numeric"                 => "Pole :attribute must be a number.",
+	"regex"                   => "Pole :attribute jest złego formatu.",
+	"required"                => "Pole :attribute jest wymagane.",
+	"required_only_on_create" => "Pole :attribute jest wymagane.",
+	"required_if"             => "Pole :attribute jest wymagane gdy :other jest :value.",
+	"required_with"           => "Pole :attribute field is required when :values is present.",
+	"required_with_all"       => "Pole :attribute field is required when :values is present.",
+	"required_without"        => "Pole :attribute field is required when :values is not present.",
+	"required_without_all"    => "Pole :attribute field is required when none of :values are present.",
+	"same"                    => "Pole :attribute and :other must match.",
+	"unique"                  => "Pole :attribute już istnieje.",
+	"url"                     => "Pole :attribute błędny format.",
+	"url_stub"                => "Pole :attribute błędny format.",
+	"url_stub_full"           => "Pole :attribute błędny format.",
+
+
+	/*
+	|--------------------------------------------------------------------------
+	| Custom Validation Attributes
+	|--------------------------------------------------------------------------
+	|
+	| Pole following language lines are used to swap attribute place-holders
+	| with something more reader friendly such as E-Mail Address instead
+	| of "email". This simply helps us make messages a little cleaner.
+	|
+	*/
+
+	'attributes'              => [],
+
+];
